d091b258c26ff28830b5dede5696a04f6773fc8d
Sergio Barbosa <sergio.alvesbarbosa@gmail.com>
2024-01-10 13:33:08 -0300

    add logo & title in hhome

--- App.js
+++ App.js
@@ -1,8 +1,43 @@
-// App.js
 import React from 'react';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Text, Image } from 'react-native';
 import Navigation from './Navigation';
 
 export default function App() {
-  return <Navigation />;
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image
+          source={require('./assets/icon.png')}
+          style={styles.logo}
+        />
+        <Text style={styles.title}>Agenda Escolar</Text>
+      </View>
+      <Navigation />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    marginTop: 55,
+    marginLeft: 20,
+  },
+  logo: {
+    width: 40, // Ajuste o tamanho conforme necessário
+    height: 40, // Ajuste o tamanho conforme necessário
+    marginRight: 10,
+  },
+  title: {
+    paddingLeft: 20,
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
